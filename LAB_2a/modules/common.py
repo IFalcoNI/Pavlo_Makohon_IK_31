c6c6e30c9e1e389c8583a91f17c6f3e6f5daeac5
FalcoN <48328425+IFalcoNI@users.noreply.github.com>
2021-10-20 19:37:24 +0300

    Fixed README

--- LAB_2a/modules/common.py
+++ LAB_2a/modules/common.py
@@ -1,5 +1,6 @@
 import datetime
 import sys
+import logging
 
 
 def get_current_date():
@@ -14,3 +15,30 @@ def get_current_platform():
     :return: current platform
     """
     return sys.platform
+
+
+def filtr_number(filtr):
+    numbers = range(0, 101)
+    if filtr == "True":
+        output = "Even number: "
+    elif filtr == "False":
+        output = "Odd number: "
+
+    for num in numbers:
+        if (filtr == "True") & (num % 2 == 0):
+            output += str(num) + " "
+        elif (filtr == "False") & (num % 2 != 0):
+            output += str(num) + " "
+    return output
+
+
+def view_array():
+    x = [1, 2, 3, 4]
+    print("Масив X[]:", x)
+    index = int(input("Enter number of element: "))
+    try:
+        print(f"X[{index}] = {x[index]}")
+    except IndexError:
+        logging.error("Out of range")
+    else:
+        logging.info("Correct!!!")
